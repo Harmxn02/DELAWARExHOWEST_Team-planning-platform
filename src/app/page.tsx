2528df1b063103e7af43e88ea2c909e4e3441984
Harman Singh <harmandualsingh@gmail.com>
2024-11-21 12:38:52 +0100

    Create route for employees and fetch data to client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,59 @@
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+
 export default function Home() {
-	return <div></div>;
+	interface Employee {
+		id: number;
+		firstname: string;
+		lastname: string;
+		email: string;
+		isAvailable: boolean;
+	}
+
+	const [employees, setEmployees] = useState<Employee[]>([]);
+
+	useEffect(() => {
+		const fetchEmployees = async () => {
+			try {
+				const data = await fetch("/api/employees");
+				const response = await data.json();
+
+				setEmployees(response.employees);
+				console.log(response.employees);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchEmployees();
+	}, []);
+
+	return (
+		<div className="m-10 space-y-20">
+			<div>
+				<h1 className="text-4xl font-bold mb-5">Employees</h1>
+				<table>
+					<thead>
+						<tr>
+							<th className="text-left min-w-[150px]">First Name</th>
+							<th className="text-left min-w-[150px]">Last Name</th>
+							<th className="text-left min-w-[300px]">Email</th>
+							<th className="text-left min-w-[150px]">Availability</th>
+						</tr>
+					</thead>
+					<tbody>
+						{employees.map((employee) => (
+							<tr key={employee.id}>
+								<td>{employee.firstname}</td>
+								<td>{employee.lastname}</td>
+								<td>{employee.email}</td>
+								<td>{employee.isAvailable ? "✅" : "❌"}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
