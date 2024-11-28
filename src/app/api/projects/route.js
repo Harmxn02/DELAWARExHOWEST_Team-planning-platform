3f596e983e995165969b1f6ce4cfe830010d60c1
Harman Singh <harmandualsingh@gmail.com>
2024-11-28 11:21:29 +0100

    Only fetch active projects

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
 	try {
 		const db = await createConnection();
-		const sql = "SELECT * FROM projects ORDER BY dateStarted DESC";
+		const sql = "SELECT * FROM projects WHERE isActive = True ORDER BY dateStarted DESC";
 		const [projects] = await db.query(sql);
 		
 		return NextResponse.json({ projects: projects });
